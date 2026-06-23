e19e1d7be9cccbc5d434e3b83b90c611219d3b92
Prashant Raj <prashantrj.4535@gmail.com>
2026-06-23 22:37:10 +0530

    date updated

--- lib/site.ts
+++ lib/site.ts
@@ -5,7 +5,7 @@ export const SITE = {
     "From LAN showdowns to online tournaments, RageX is where gamers compete, connect, and level up",
   instagramUrl: "https://www.instagram.com/rageee__x/",
   registerUrl: "https://docs.google.com/forms/d/e/1FAIpQLSc4d26ngNuwQJeWyGOfwiM-QeDyPmyry4r-RbDT7bDYI82OKg/viewform?usp=dialog",
-  date: "TBA",
+  date: "5th July 2026",
   venue: "Hexa Gaming",
   prizePool: [
     "Valorant 5v5: Up to 5K",
